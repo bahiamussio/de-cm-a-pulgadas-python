462c3f9dbf4d9882402fc8914ef971ebeb730f1a
Cati <catita.bm@hotmail.com>
2024-12-19 08:42:37 -0300

    git remote add origin git@github.com:bahiamussio/de-cm-a-pulgadas-python.git

---
+++ cm-a-in.py
@@ -0,0 +1,10 @@
+ # Paso 1:  Definir cuántos centímetros entran en una pulgada.
+pulgada = 2.54
+
+# Paso 2:  Solicitar al usuario que ingrese las medidas de la pieza del mueble en cm.
+medidas_cm = float(input("Por favior, ingrese las medidas de la pieza en centímetros para convertirla a pulgadas: "))
+# Paso 3: Realizar la conversión de centímetros a pulgadas.
+medidas_pulgadas = medidas_cm * pulgada
+
+# Paso 4: Mostrarle al usuario las medidas convertidas a in.
+print("Las medidas en pulgadas de la pieza ingresada son:", medidas_pulgadas)
